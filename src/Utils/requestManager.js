f553b8419eedbb95c864f119e532606052809594
thundebolt <its.saravanan.rajendran@gmail.com>
2021-03-07 13:58:44 +0530

    http to https

--- src/Utils/requestManager.js
+++ src/Utils/requestManager.js
@@ -17,7 +17,7 @@ const RequestManager = (
         });
 };
 
-const apiHost = "http://api.kaanoli.me:7500/";
+const apiHost = "https://api.kaanoli.me:7500/";
 
 const ApiHandler = {
     subscribe: (data, successCallBack, errorCallBack) => {
